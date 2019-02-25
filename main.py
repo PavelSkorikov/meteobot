1ae6a3770eff5eedf24ca2be4ca1218c656c6691
PavelSkorikov <spg75@yandex.ru>
2019-02-25 21:56:48 +0300

    6

--- main.py
+++ main.py
@@ -1,40 +1,31 @@
 import json, requests
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
-from kivy.uix.label import Label
-from kivy.uix.image import Image, AsyncImage
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.checkbox import CheckBox
-from kivy.properties import StringProperty
-from kivy.uix.gridlayout import GridLayout
-from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import Screen, ScreenManager
+from threading import Thread
 
 from kivy.config import  Config
-Config.set('graphics', 'resizable', '1')
+Config.set('graphics', 'resizable', '0')
 Config.set('graphics', 'width', '1024')
 Config.set('graphics', 'height', '720')
+Config.set('kivy', 'exit_on_escape', '0')
+
+class Gerenciador(ScreenManager):
+    pass
 
 
-class MainForm(BoxLayout):
-    search_input = ObjectProperty()
-    info = ObjectProperty()
-    date = ObjectProperty()
-    tmax = ObjectProperty()
-    tmin = ObjectProperty()
-    wind = ObjectProperty()
-    condition = ObjectProperty()
-    totalprecip = ObjectProperty()
-    avgvis = ObjectProperty()
-    avghumidity = ObjectProperty()
+class MainForm(Screen):
 
     def search_location(self):
-        location = self.search_input.text
+        location = self.search_input
         if location == 'Москва':
             location = 'Moscow'
         url_apixu = 'http://api.apixu.com/v1/forecast.json?key=209e4be4a40a421ab34160306191302&q=%s&days=10' % (
             location)
-        response = requests.get(url_apixu)
+        try:
+            response = requests.get(url_apixu)
+        except ConnectionError:
+            self.info.text = 'Ошибка, проверьте соединение с интернетом!'
+            return
         if response.status_code == 400:
             self.info.text = 'Такое место не найдено'
             for i in range(7):
@@ -52,7 +43,7 @@ class MainForm(BoxLayout):
             self.info.text = nf
             for i in range(7):
                 d = w['forecast']['forecastday'][i]['date'].replace('-','.')
-                self.date[i].text = d[5:]
+                self.date[i].text = d[8:10] + d[4:7]
                 self.tmax[i].text = str(w['forecast']['forecastday'][i]['day']['maxtemp_c'])
                 self.tmin[i].text = str(w['forecast']['forecastday'][i]['day']['mintemp_c'])
                 self.wind[i].text = str(w['forecast']['forecastday'][i]['day']['maxwind_mph'])
@@ -60,11 +51,18 @@ class MainForm(BoxLayout):
                 self.totalprecip[i].text = str(w['forecast']['forecastday'][i]['day']['totalprecip_mm'])
                 self.avgvis[i].text = str(w['forecast']['forecastday'][i]['day']['avgvis_km'])
                 self.avghumidity[i].text = str(w['forecast']['forecastday'][i]['day']['avghumidity'])
-
-            print(w['current']['last_updated'], w['current']['temp_c'], w['current']['condition']['icon'])
+                self.voshod[i].text = str(w['forecast']['forecastday'][i]['astro']['sunrise'])
+                self.zakat[i].text = str(w['forecast']['forecastday'][i]['astro']['sunset'])
+    def meteo_worker(self):
+        t = Thread(target=self.search_location)
+        t.start()
+        t.join()
+    def prog_info(self):
+        self.info.text = 'Эта программа создана в целях изучения python + kivy. Автор - Скориков П.Г. spg75@yandex.ru'
 
 class MeteoBotApp(App):
-    pass
+    def build(self):
+        return Gerenciador()
 
 if __name__ == "__main__":
     MeteoBotApp().run()
